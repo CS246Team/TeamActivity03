25ac145f06920d3b0d2bffcf474bd61697a1d8ab
arrowalker3 <arrowalker3@gmail.com>
2020-05-06 11:25:18 -0600

    Add files via upload

--- Game.java
+++ Game.java
@@ -1,6 +1,9 @@
-package com.company;
+package ta03;
 
 import com.google.gson.Gson;
+
+import java.io.*;
+import java.util.Scanner;
 
 public class Game {
     Player player;
@@ -9,13 +12,49 @@ public class Game {
         player = p;
     }
 
-    public void saveGame(){
-        Gson g = new Gson();
-        g.toJson(player);
+    public void createFile() {
+        try {
+            File f = new File("newGame.txt");
+            if (f.createNewFile()) {
+                System.out.println("Created new file\n");
+            } else {
+                System.out.println("File already exists.\n");
+            }
+        } catch (IOException e) {
+            System.out.println("Error creating file.\n");
+        }
     }
 
-    public static void loadGame(String filename){
+    public void saveGame(){
         Gson g = new Gson();
-        g.
+        String myPlayer = g.toJson(this);
+        createFile();
+        FileWriter b = null;
+        try {
+            b = new FileWriter("newGame.txt");
+            b.write(myPlayer);
+            b.close();
+            System.out.println("Wrote to the file.\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static Game loadGame(String filename){
+        String json = "";
+        Game newObj = null;
+        try {
+            File myObj = new File(filename);
+            Scanner reader = new Scanner(myObj);
+            json = reader.nextLine();
+            Gson g = new Gson();
+            newObj = g.fromJson(json, Game.class);
+            reader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+
+        return newObj;
     }
 }
--- Main.java
+++ Main.java
@@ -1,10 +1,30 @@
-package com.company;
-
-import com.google.gson.Gson;
+package ta03;
 
 public class Main {
+    public Main() {
+    }
 
     public static void main(String[] args) {
-	    Gson g = new Gson();
+        Player p = new Player();
+
+        // Stretch Challenge: give player equipment
+        p.equipment.put("Axe", 5);
+        p.equipment.put("Speed Boat", 10);
+        p.equipment.put("Laser gun", 900);
+
+        Game g = new Game(p);
+
+        // Set values for player in the game
+        p.setGold(5);
+        p.setHealth(8);
+        p.setMana(3);
+        p.setName("Trey");
+
+        // Save the game
+        g.saveGame();
+
+        // Load game into brand new game object
+        Game g2 = Game.loadGame("newGame.txt");
+        g2.player.displayPlayerValues();
     }
 }
